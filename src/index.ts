9a2464431afe3143801cc4f40037673f33d1930a
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-09-15 10:54:04 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,44 @@
+import {
+  JupyterFrontEnd,
+  JupyterFrontEndPlugin
+} from '@jupyterlab/application';
+
+import { ISettingRegistry } from '@jupyterlab/settingregistry';
+
+import { requestAPI } from './handler';
+
+/**
+ * Initialization data for the @jupyter/sql-cell extension.
+ */
+const plugin: JupyterFrontEndPlugin<void> = {
+  id: '@jupyter/sql-cell:plugin',
+  description: 'A JupyterLab extension to run SQL in notebook dedicated cells',
+  autoStart: true,
+  optional: [ISettingRegistry],
+  activate: (app: JupyterFrontEnd, settingRegistry: ISettingRegistry | null) => {
+    console.log('JupyterLab extension @jupyter/sql-cell is activated!');
+
+    if (settingRegistry) {
+      settingRegistry
+        .load(plugin.id)
+        .then(settings => {
+          console.log('@jupyter/sql-cell settings loaded:', settings.composite);
+        })
+        .catch(reason => {
+          console.error('Failed to load settings for @jupyter/sql-cell.', reason);
+        });
+    }
+
+    requestAPI<any>('get-example')
+      .then(data => {
+        console.log(data);
+      })
+      .catch(reason => {
+        console.error(
+          `The jupyter_sql_cell server extension appears to be missing.\n${reason}`
+        );
+      });
+  }
+};
+
+export default plugin;
