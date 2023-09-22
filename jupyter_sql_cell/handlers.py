bdb493d78fcd350736e6053027568bf73f429dd8
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-09-22 15:54:14 +0200

    Use APIHAndler instead of JupyterHandler

--- jupyter_sql_cell/handlers.py
+++ jupyter_sql_cell/handlers.py
@@ -1,12 +1,12 @@
 import json
 
-from jupyter_server.base.handlers import APIHandler, JupyterHandler
+from jupyter_server.base.handlers import APIHandler
 import tornado
 
 from .sqlconnector import SQLConnector
 
 
-class ExecuteHandler(JupyterHandler):
+class ExecuteHandler(APIHandler):
     # The following decorator should be present on all verb methods (head, get, post,
     # patch, put, delete, options) to ensure only authorized user can request the
     # Jupyter server
