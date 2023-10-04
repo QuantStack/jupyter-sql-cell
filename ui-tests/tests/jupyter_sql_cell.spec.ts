00ab00ce48c081c276ef031de75a32a7b9b8a0bc
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-10-04 17:05:45 +0200

    Add integration tests on cell toolbar buttons

--- ui-tests/tests/jupyter_sql_cell.spec.ts
+++ ui-tests/tests/jupyter_sql_cell.spec.ts
@@ -6,18 +6,59 @@ import { expect, test } from '@jupyterlab/galata';
  */
 test.use({ autoGoto: false });
 
-test('should emit an activation console message', async ({ page }) => {
-  const logs: string[] = [];
-
-  page.on('console', message => {
-    logs.push(message.text());
-  });
-
+test('cell-toolbar should contains buttons for raw cells only', async ({
+  page
+}) => {
   await page.goto();
+  await page.notebook.createNew();
+  await page.notebook.addCell('code', '');
+
+  // The cell toolbar should not contain SQL buttons for code cells
+  await expect(page.locator('.jp-cell-toolbar')).toHaveCount(1);
 
   expect(
-    logs.filter(
-      s => s === 'JupyterLab extension @jupyter/sql-cell is activated!'
-    )
-  ).toHaveLength(1);
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+  ).not.toBeVisible();
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:execute"]')
+  ).not.toBeVisible();
+
+  // The cell toolbar should not contain SQL buttons for markdown cells
+  await page.notebook.setCellType(1, 'markdown');
+
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+  ).not.toBeVisible();
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:execute"]')
+  ).not.toBeVisible();
+
+  // The cell toolbar should not contain SQL buttons for raw cells
+  await page.notebook.setCellType(1, 'raw');
+
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+  ).toBeVisible();
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:execute"]')
+  ).toBeVisible();
+});
+
+test('cell-toolbar execute button should be enabled for sql cell only', async ({
+  page
+}) => {
+  await page.goto();
+  await page.notebook.createNew();
+  await page.notebook.addCell('raw', '');
+
+  expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:execute"]')
+  ).toBeDisabled();
+
+  await page
+    .locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+    .click();
+  await expect(
+    page.locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:execute"]')
+  ).toBeEnabled();
 });
