c69d1bcdae7acbc3ae269a65b2aeb2374b893ef6
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-10-05 14:04:38 +0200

    Adds test

--- ui-tests/tests/jupyter_sql_cell.spec.ts
+++ ui-tests/tests/jupyter_sql_cell.spec.ts
@@ -69,3 +69,63 @@ test.describe('cell toolbar', () => {
     ).toBeEnabled();
   });
 });
+
+test.describe('cell factory', () => {
+  test.beforeEach(async ({ page, request, tmpPath }) => {
+    const contents = galata.newContentsHelper(request);
+    await contents.uploadFile(
+      path.resolve(__dirname, `./notebooks/${fileName}`),
+      `${tmpPath}/${fileName}`
+    );
+    await page.notebook.openByPath(`${tmpPath}/${fileName}`);
+    await page.notebook.activate(fileName);
+  });
+
+  test.afterEach(async ({ request, tmpPath }) => {
+    const contents = galata.newContentsHelper(request);
+    await contents.deleteDirectory(tmpPath);
+  });
+
+  test('SQL cells only should have jp-SqlCell class', async ({ page }) => {
+    const cells = page.locator('.jp-Notebook .jp-Cell');
+
+    await expect(cells).toHaveCount(3);
+
+    await expect(cells.nth(0)).not.toHaveClass(/jp-SqlCell/);
+    await expect(cells.nth(1)).not.toHaveClass(/jp-SqlCell/);
+    await expect(cells.nth(2)).not.toHaveClass(/jp-SqlCell/);
+
+    await (await page.notebook.getCellInput(2))?.click();
+    await page
+      .locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+      .click();
+
+    await expect(cells.nth(2)).toHaveClass(/jp-SqlCell/);
+  });
+
+  test('SQL cells should have SQL mimetype', async ({ page }) => {
+    const cells = page.locator('.jp-Notebook .jp-Cell');
+
+    // SQl cells should have SQL language.
+    await (await page.notebook.getCellInput(2))?.click();
+    await page
+      .locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+      .click();
+
+    await expect(
+      cells.nth(2).locator('div[contenteditable="true"]')
+    ).toHaveAttribute('data-language', 'sql');
+
+    // Raw cells not have codemirror language.
+    await page
+      .locator('.jp-cell-toolbar [data-command="jupyter-sql-cell:switch"]')
+      .click();
+
+    expect(
+      await cells
+        .nth(2)
+        .locator('div[contenteditable="true"]')
+        .getAttribute('data-language')
+    ).toBe(null);
+  });
+});
