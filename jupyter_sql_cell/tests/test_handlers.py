23eeb398aa983b049a4be61539d648e88fc728d2
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-10-10 09:53:07 +0200

    update tests

--- jupyter_sql_cell/tests/test_handlers.py
+++ jupyter_sql_cell/tests/test_handlers.py
@@ -12,16 +12,44 @@ async def test_get_example(jp_fetch):
         "data": "This is /jupyter-sql-cell/get-example endpoint!"
     }
 
-async def test_execute(jp_fetch):
+
+"""
+Should load and query the default database when none has been provided in config.
+"""
+async def test_execute_default(jp_fetch):
+    query = "SELECT Abbreviation FROM world WHERE Country='France'"
     response = await jp_fetch(
         "jupyter-sql-cell",
         "execute",
-        body=json.dumps({"query": "SELECT Abbreviation FROM world WHERE Country='France'"}),
+        body=json.dumps({
+            "query": query,
+            "id": "0"
+        }),
         method="POST"
     )
 
     assert response.code == 200
     payload = json.loads(response.body)
-    assert payload == {
-        "data": [{"Abbreviation": "FR"}]
-    }
+    assert list(payload.keys()) == ["data", "id", "query", "url"]
+    assert payload["data"] == [{"Abbreviation": "FR"}]
+    assert payload["query"] == query
+    assert payload["id"] == "0"
+
+
+"""
+Should load and query the default database when none has been provided.
+Missing ID in request should fall back on first database.
+"""
+async def test_execute_no_id(jp_fetch):
+    response = await jp_fetch(
+        "jupyter-sql-cell",
+        "execute",
+        body=json.dumps({
+            "query": "SELECT Abbreviation FROM world WHERE Country='France'"
+        }),
+        method="POST"
+    )
+
+    assert response.code == 200
+    payload = json.loads(response.body)
+    assert payload["data"] == [{"Abbreviation": "FR"}]
