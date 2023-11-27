4589f365f78cc166f130c0546d0413643d88a02e
Nicolas Brichet <nicolas.brichet@quantstack.net>
2023-11-27 15:30:35 +0100

    select value reflect the magic line

--- src/widgets.tsx
+++ src/widgets.tsx
@@ -1,60 +1,104 @@
 import { ICellModel } from '@jupyterlab/cells';
-import * as React from 'react';
+import { ReactWidget } from '@jupyterlab/ui-components';
+
+import React from 'react';
 
 import { MAGIC } from './common';
 import { Database } from './databases';
 import { IDatabasesPanel } from './sidepanel';
 
 /**
- * A field including a select to associate a database.
+ * Database select widget.
  */
-export const DatabaseSelect = (options: {
-  cellModel: ICellModel | null;
-  databasesPanel: IDatabasesPanel;
-}) => {
-  const onChange = (event: React.FormEvent) => {
+export class DatabaseSelect extends ReactWidget {
+  constructor(options: {
+    cellModel: ICellModel | null;
+    databasesPanel: IDatabasesPanel;
+  }) {
+    super();
+    this._cellModel = options.cellModel;
+    this._databasesPanel = options.databasesPanel;
+  }
+
+  onChange = (event: React.FormEvent) => {
     const selection = (event.target as HTMLSelectElement).value;
-    const database = options.databasesPanel.get_database(selection);
-    if (options.cellModel && database) {
-      Private.changeDatabase(options.cellModel, database);
+    const database = this._databasesPanel.get_database(selection);
+    if (this._cellModel && database) {
+      Private.setDatabaseUrl(this._cellModel, database);
     }
   };
-  return (
-    <div>
-      <label>
-        Database:&nbsp;
-        <select
-          onChange={onChange}
-          className={'jp-sqlcell-select'}
-          disabled={options.cellModel?.type !== 'code'}
-        >
-          <option> - </option>
-          {options.databasesPanel?.databases.map(database => {
-            return <option>{database.alias}</option>;
-          })}
-        </select>
-      </label>
-    </div>
-  );
-};
+
+  render(): JSX.Element {
+    const defaultValue = ' - ';
+    let currentDatabase = defaultValue;
+    const url = Private.getDatabaseUrl(this._cellModel);
+    const aliases: string[] = [];
+    this._databasesPanel?.databases.forEach(database => {
+      aliases.push(database.alias);
+      if (database.url === url) {
+        currentDatabase = database.alias;
+      }
+    });
+    return (
+      <div>
+        <label>
+          Database:&nbsp;
+          <select
+            onChange={this.onChange}
+            className={'jp-sqlcell-select'}
+            disabled={this._cellModel?.type !== 'code'}
+          >
+            <option disabled selected={currentDatabase === defaultValue}>
+              {defaultValue}
+            </option>
+            ;
+            {aliases.map(alias => {
+              return (
+                <option selected={currentDatabase === alias}>{alias}</option>
+              );
+            })}
+          </select>
+        </label>
+      </div>
+    );
+  }
+
+  private _cellModel: ICellModel | null;
+  private _databasesPanel: IDatabasesPanel;
+}
 
 /**
  * The private namespace.
  */
 namespace Private {
+  export function getDatabaseUrl(
+    cellModel: ICellModel | null
+  ): string | undefined {
+    if (!cellModel) {
+      return;
+    }
+    const regexp = new RegExp(`^${MAGIC}\\s+((?!\\s).*)`);
+    const magicLine = cellModel.sharedModel.source.split('\n')[0];
+    const match = magicLine.match(regexp);
+    if (match && match.length > 1) {
+      return match[1];
+    }
+  }
+
   /**
    * Update the contents of the magic line of the cell, accordingly to the selection.
    *
    * @param cellModel - the model of the cell whose contents are to be modified.
    * @param database - the selected database.
    */
-  export function changeDatabase(
+  export function setDatabaseUrl(
     cellModel: ICellModel,
-    database: Database
+    database: Database | undefined
   ): void {
     let magicLine = MAGIC;
-    magicLine += ` ${database.url}`;
-
+    if (database) {
+      magicLine += ` ${database.url}`;
+    }
     const source = cellModel.sharedModel.source;
     const sourceArray = source.split('\n');
     sourceArray[0] = magicLine;
